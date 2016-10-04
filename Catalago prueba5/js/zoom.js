cab4fba490a40a420eb8e66a112f7a142f268ae1
Juan Carlos Guerrero Fernandez <juancarlos.gf02@gmail.com>
2016-10-04 18:08:27 -0500

    prueba zoom
    zoom con script

---
+++ Catalago prueba5/js/zoom.js
@@ -0,0 +1,46 @@
+//funciones para llamar en el index
+		$(document).ready(function(){
+			$('#ex1').zoom({ on:'click' });			 
+		});
+		
+
+		$(document).ready(function(){
+			$('#ex2').zoom({ on:'click' });			 
+		});
+	
+
+		$(document).ready(function(){
+			$('#ex3').zoom({ on:'click' });			 
+		});
+
+	$(document).ready(function(){
+			$('#ex4').zoom({ on:'click' });			 
+		});
+
+	$(document).ready(function(){
+			$('#ex5').zoom({ on:'click' });			 
+		});
+        
+	$(document).ready(function(){
+			$('#ex6').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex7').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex8').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex9').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex10').zoom({ on:'click' });			 
+		});
+    $(document).ready(function(){
+			$('#ex11').zoom({ on:'click' });			 
+		});
+    $(document).ready(function(){
+			$('#ex12').zoom({ on:'click' });			 
+		});
+
+
