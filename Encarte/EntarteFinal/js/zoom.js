666858cbbe7e4495bbe708abcd243bd23e1b883f
Juan Carlos Guerrero Fernandez <juancarlos.gf02@gmail.com>
2016-10-25 10:41:58 -0500

    Organizado y agregados

---
+++ Encarte/EntarteFinal/js/zoom.js
@@ -0,0 +1,80 @@
+//funciones para llamar en el index
+		$(document).ready(function(){
+			$('#ex1').zoom({ on:'click' });	});
+		
+
+		$(document).ready(function(){
+			$('#ex2').zoom({ on:'click' });			 
+		});
+	
+
+		$(document).ready(function(){
+			$('#ex3').zoom({ on:'click' });			 
+		});
+
+	$(document).ready(function(){
+			$('#ex4').zoom({ on:'click' });			 
+		});
+
+	$(document).ready(function(){
+			$('#ex5').zoom({ on:'click' });			 
+		});
+        
+	$(document).ready(function(){
+			$('#ex6').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex7').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex8').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex9').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex10').zoom({ on:'click' });			 
+		});
+    $(document).ready(function(){
+			$('#ex11').zoom({ on:'click' });			 
+		});
+    $(document).ready(function(){
+			$('#ex12').zoom({ on:'click' });			 
+		}); 
+		
+	$(document).ready(function(){
+			$('#ex13').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex14').zoom({ on:'click' });			 
+		});  
+	$(document).ready(function(){
+			$('#ex15').zoom({ on:'click' });			 
+		});
+
+$(document).ready(function(){
+			$('#ex16').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex17').zoom({ on:'click' });			 
+		});  
+	$(document).ready(function(){
+			$('#ex18').zoom({ on:'click' });			 
+		});
+$(document).ready(function(){
+			$('#ex19').zoom({ on:'click' });			 
+		});
+	$(document).ready(function(){
+			$('#ex20').zoom({ on:'click' });			 
+		});  
+	$(document).ready(function(){
+			$('#ex21').zoom({ on:'click' });			 
+		});
+
+	$(document).ready(function(){
+			$('#ex22').zoom({ on:'click' });			 
+		});  
+	$(document).ready(function(){
+			$('#ex24').zoom({ on:'click' });			 
+		});
+
